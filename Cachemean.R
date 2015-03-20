626162e1c4b7d9c6c446d72bd7028432280fd72a
Marcelita <marcelaotero14@gmail.com>
2015-03-20 00:33:36 -0300

    Create Cachemean.R

---
+++ Cachemean.R
@@ -0,0 +1,12 @@
+··MakeVector <- function(){
+
+ was complicated by me because I gave x= matrix (1:20, 2, 3) so took ages to process.
+}
+
+
+··Cachemean <- function(){
+
+ Immeaditely I had to stop this process but made it larger.
+}
+·· A mistake was made because in the mean time I run Complete.R function. 
+Enter file contents here
